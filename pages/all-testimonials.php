3f198c9a0120c379d469e4703f1e0b20e6a601ed
Purnendu Kar <purnendu.kar8@gmail.com>
2019-02-08 16:34:35 +0530

    all testimonial page dynamic

--- pages/all-testimonials.php
+++ pages/all-testimonials.php
@@ -63,70 +63,21 @@ else
       <main class="all_testimonials__main">
         <div class="all_testimonials__main__sep"></div>
         <h1 class="all_testimonials__main__title">what students say about us</h1>
-
+        <?php $res=$conn->query("SELECT * FROM `testimonial`")?>
         <div class="all_testimonials__main__container">
-
+          <?php while($row=$res->fetch_assoc()){?>
           <div class="all_testimonials__main__item">
             <div class="all_testimonials__main__item__img">
-              <img src="http://1.bp.blogspot.com/-hNC-oT6f-fY/TeXxO26yjvI/AAAAAAAAAOY/qfkOqdKkBi8/s1600/platon-photographer-putin-man-of-the-year-portrait.jpg" alt="Image of student" />
+              <img src="<?php echo ".".$row['img_src']?>" alt="Image Can't Load" />
             </div>
             <div class="all_testimonials__main__item__content">
-              <h4>andrew laedis</h4>
-              <h5>nmims</h5>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum optio labore reiciendis atque, eius</p>
+              <h4><?php echo urldecode($row['head']);?></h4>
+              <h5><?php echo urldecode($row['para']);?></h5>
+              <p><?php echo urldecode($row['about']);?></p>
             </div>
           </div>
-          <div class="all_testimonials__main__item">
-            <div class="all_testimonials__main__item__img">
-              <img src="http://www.aadhu.com/wp-content/uploads/2018/01/google-camera-port-with-portrait-mode-now-works-on-samsung-galaxy-s7-s8-and-note-8-exynos.jpg" alt="Image of student" />
-            </div>
-            <div class="all_testimonials__main__item__content">
-              <h4>dazai osamu</h4>
-              <h5>bharati vidyapeeth</h5>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum optio labore reiciendis atque, eius distinctio sit eveniet voluptatibus. .</p>
-            </div>
-          </div>
-          <div class="all_testimonials__main__item">
-            <div class="all_testimonials__main__item__img">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq1CDNNwbHat-PN5RsYZjfYMcYCLjtWo_oC3NSpHOPnv3YoJ1c" alt="Image of student" />
-            </div>
-            <div class="all_testimonials__main__item__content">
-              <h4>kunikida</h4>
-              <h5>assam downtown university</h5>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum optio labore reiciendis at.</p>
-            </div>
-          </div>
-          <div class="all_testimonials__main__item">
-            <div class="all_testimonials__main__item__img">
-              <img src="http://www.chennaiportraitphotography.com/wp-content/uploads/2015/08/IMG_0013_1.jpg" alt="Image of student" />
-            </div>
-            <div class="all_testimonials__main__item__content">
-              <h4>madara uchiha</h4>
-              <h5>scdl</h5>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum optio labore reiciendis atque, eius distinctio sit.</p>
-            </div>
-          </div>
-          <div class="all_testimonials__main__item">
-            <div class="all_testimonials__main__item__img">
-              <img src="https://akm-img-a-in.tosshub.com/indiatoday/images/photogallery/201708/thumb_082117102744.jpg" alt="Image of student" />
-            </div>
-            <div class="all_testimonials__main__item__content">
-              <h4>madara uchiha</h4>
-              <h5>scdl</h5>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum optio labore reiciendis atque, eius distinctio sit eveniet </p>
-            </div>
-          </div>
-          <div class="all_testimonials__main__item">
-            <div class="all_testimonials__main__item__img">
-              <img src="https://media.gettyimages.com/photos/indian-defense-scientist-apj-abdul-kalam-poses-for-a-portrait-in-this-picture-id1127521?s=612x612" alt="Image of student" />
-            </div>
-            <div class="all_testimonials__main__item__content">
-              <h4>madara uchiha</h4>
-              <h5>scdl</h5>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum optio labore reiciendis atque, eius distinctio sit eveniet voluptat.</p>
-            </div>
-          </div>
-
+          <?php } ?>
+          
         </div>
       </main>
 
