7257830d5f3874007bc092caa1b79ecffe9ad106
Purnendu Kar <purnendu.kar8@gmail.com>
2019-02-11 14:05:19 +0530

    blog page from Akil ahmed

---
+++ blogPage/app.js
@@ -0,0 +1,13 @@
+const mySiema = new Siema({
+    duration: 500,
+    loop: true,
+    easing: 'ease-out',
+    draggable: false,
+    autoHover: true,
+  });
+  
+  // listen for keydown event
+  setInterval(() => mySiema.next(), 8000)
+
+  document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+  document.querySelector('.next').addEventListener('click', () => mySiema.next());
