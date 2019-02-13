b67fd49fb53c6db746cf48b48c888ffaf3b7401b
Purnendu Kar <purnendu.kar8@gmail.com>
2019-02-13 17:46:03 +0530

    update

--- includes/user_track_cookie.php
+++ includes/user_track_cookie.php
@@ -1,7 +1,7 @@
 <?php
-
 include "./mysql_connect.php";
 $conn=connect_mysql();
+try{
 if(isset($_COOKIE['e_id'])){
 $sql="select * from user_session where email='".$_COOKIE['e_id']."'";
 $id=$conn->query($sql)->fetch_assoc()['id'];
@@ -9,7 +9,9 @@ $id_l=$conn->query("select id from user_session_track where (user_id=".$id." and
 
 update_checker($_POST['ra']);
 }
-
+}catch(Exception $e){
+    
+}
 function update_checker($r){
         if($GLOBALS['conn']->query("UPDATE `user_session_track` SET `checker`= ".$r." WHERE id=".$GLOBALS['id_l']))
             echo "here ";
