371cbaf95e89bea35c36a69a87c770ef0c5a1050
Purnendu Kar <purnendu.kar8@gmail.com>
2019-02-23 17:39:22 +0530

    pdf maker update

--- pages/test.php
+++ pages/test.php
@@ -4,13 +4,26 @@ require_once 'dompdf/autoload.inc.php';
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
+include "../includes/mysql_connect.php";
+$conn=connect_mysql();
+$user_info=$conn->query("select * from register_user where email='".urldecode($_COOKIE['email'])."'")->fetch_assoc();
+$str='<div><h1 style="font-size:30px;">'.$user_info['f_name'].' '.$user_info['l_name'].'</h1><p style="font-size:18px;">'.$user_info['email'].'</p><p style="font-size:18px;">'.$user_info['phn_no'].'</p></div>';
+$user_info=$conn->query("select * from user_education where email='".urldecode($_COOKIE['email'])."'");
+if($row=$user_info->fetch_assoc()){
+    $str.='<hr style="border-color:#9A9A9A;"><table><tr><td style="color:#0387B0;font-size:25px;width:200px;vertical-align:top;"><p>Education</p></td><td><div style="color:#6C6C6C;"><p style="font-size:20px;color:black">'.urldecode($row['degree']).' ('.$row['join_year'].' - '.$row['complete_year'].')</p>'.urldecode($row['organisation']).'</div>';
+    while($row=$user_info->fetch_assoc()){
+        $str.='<div style="color:#6C6C6C;"><p style="font-size:20px;color:black;">'.urldecode($row['degree']).' ('.$row['join_year'].' - '.$row['complete_year'].')</p>'.urldecode($row['organisation']).'</div>';
+    }
+    $str.='</td></tr></table>';
+
+}
+
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-$t="hello world";
-$dompdf->loadHtml('<h1 id="hi" style="background-color:red;">hello world</h1><hr width="100%"><p>sadwe <br> ads'.$t.'</p>');
+$dompdf->loadHtml($str);
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+$dompdf->setPaper('A4', 'horizontal');
 
 // Render the HTML as PDF
 $dompdf->render();
