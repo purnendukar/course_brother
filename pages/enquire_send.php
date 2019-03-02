6375ca37d3957816b3b5f220f1669a3e71262e30
Purnendu Kar <purnendu.kar8@gmail.com>
2019-03-02 14:56:43 +0530

    mail update

--- pages/enquire_send.php
+++ pages/enquire_send.php
@@ -13,47 +13,75 @@
         $str.=" halting from ".strtoupper($city).",";
     }
     $str.=" and I would like to discuss about ".strtoupper($course)." course. My email address is ".$email.", and my phone number is ".strtoupper($phn).". THANK YOU.";
-    echo $str;
     $e=$conn->query("SELECT * FROM `contact_info` where id=2")->fetch_assoc()['info'];
+    
+    
+
+    ini_set('SMTP','smtp.zoho.com');
+    ini_set('smtp_port',465);
+    ini_set('sendmail_from', 'shyam.d@coursebrother.com');
+    
+    //define the receiver of the email
+    $to = 'rijusuman1996@gmail.com';
+    //define the subject of the email
+    $subject = 'Test for title'; 
+    //define the message to be sent. Each line should be separated with \n
+    $message = 'Message to send'; 
+    
+    //define the headers we want passed. Note that they are separated with \r\n
+    $headers = 'From: shyam.d@coursebrother.com\r\n';
+    
+    //send the email
+    $mail_sent = mail($to, $subject, $message, $headers);
+    mail($to, $subject, $message, $headers);
+    
+    //if the message is sent successfully print "Mail sent correctly". Otherwise print "Mail failed" 
+    echo $mail_sent ? "Mail sent" : "Mail failed";
+    
+    
+//   $to = "rijusuman1996@gmail.com"; // <– replace with your address here
+//   $subject = "Test mail";
+//   $message = "Hello! This is a simple test email message.";
+//   $from = "ghonta@coursebrother.com";
+//   $headers = "From:" . $from;
+//   mail($to,$subject,$message,$headers);
 
 
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
-require '../vendor/autoload.php';
+// //Load Composer's autoloader
+// require '../vendor/autoload.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.zoho.com';                        // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'contact@coursebrother.com';        // SMTP username
-    $mail->Password = 'Shyam28#';                         // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+// $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+// try {
+//     //Server settings
+//     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+//     $mail->isSMTP();                                      // Set mailer to use SMTP
+//     $mail->Host = 'smtp.zoho.com';                        // Specify main and backup SMTP servers
+//     $mail->SMTPAuth = true;                               // Enable SMTP authentication
+//     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+//     $mail->Port = 465;                                    // TCP port to connect to
+//     $mail->Username = 'contact@coursebrother.com';        // SMTP username
+//     $mail->Password = 'Shyam82#';                         // SMTP password
 
-    //Recipients
-    $mail->setFrom('contact@coursebrother.com', 'Mailer');
-    $mail->addAddress('shyam.d@gmail.com');
+//     //Recipients
+//     $mail->setFrom('suman@coursebrother.com', 'Mailer');
+//     $mail->addAddress('rijusuman1996@gmail.com');
 
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Enquiry';
-    $mail->Body    = $str;
+//     //Content
+//     $mail->isHTML(true);                                  // Set email format to HTML
+//     $mail->Subject = 'Enquiry';
+//     $mail->Body    = $str;
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
+//     $mail->send();
+//     echo 'Message has been sent';
+// } catch (Exception $e) {
+//     echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+// }
 
-    mail($e,"Enquiry",$str,"From: ".$email);
     $t=explode("/",$_GET['url']);
-    //header("Location: ".$t[count($t)-1]);
     
 ?>
